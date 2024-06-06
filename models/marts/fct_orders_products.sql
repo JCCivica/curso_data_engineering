29c8874cad16b93dc1dd15798890c4c129ec8c4e
Juan Ramírez <museogrx@gmail.com>
2024-06-06 17:36:18 +0000

    aa

--- models/marts/fct_orders_products.sql
+++ models/marts/fct_orders_products.sql
@@ -1,13 +1,11 @@
 WITH order_items AS (
-    SELECT * 
-    FROM {{ref('stg_sql_server_dbo__order_items')}}
+    SELECT
+        order_id,
+        product_id,
+        quantity,
+        COUNT(product_id)OVER(PARTITION BY order_id) AS pr
+    FROM {{ ref('stg_sql_server_dbo__order_items') }}
 ),
-
-order_summary AS (
-    SELECT * 
-    FROM {{ ref('int_order_summary') }}
-),
-
 
 products AS (
     SELECT *
@@ -16,23 +14,33 @@ products AS (
 
 orders AS (
     SELECT *
-    FROM {{ ref('stg_sql_server__orders') }}
+    FROM {{ ref('stg_sql_server_dbo__orders') }} 
 ),
 
 final AS (
     SELECT
-        ROW_NUMBER()OVER(PARTITION BY os.order_id ORDER BY os.product_id) AS _ROW,
-        os.order_id,
-        os.product_id,
+        --ROW_NUMBER()OVER(PARTITION BY oi.order_id ORDER BY oi.product_id) AS _ROW,
+        o.order_id,
         o.address_id,
-        os.total_quantity,
-        p.price_dollars,
-        p.price_dollars * os.total_quantity AS total_price_per_product
+        o.user_id,
+        o.promo_id,
+        o.created_at_utc,
+        o.shipping_service_id,
+        o.tracking_id,
+        oi.product_id,
+        o.status_id AS shipping_status_id,
+        oi.quantity AS product_quantity,
+        ROUND(p.price_dollars * oi.quantity, 2) AS total_price_per_product,
+        o.order_total_dollars/pr AS order_total,
+        o.order_cost_dollars/pr AS order_cost,
+        o.shipping_cost_dollars/pr AS shipping_cost,
+        ROUND(((o.order_total_dollars - (o.shipping_cost_dollars + o.order_cost_dollars))/pr),2) AS discount
     FROM orders o 
-    JOIN order_summary os
-    ON o.order_id = os.order_id
-    JOIN products p 
-    ON os.product_id = p.product_id
+    LEFT JOIN order_items oi
+    ON o.order_id = oi.order_id
+    LEFT JOIN products p 
+    ON oi.product_id = p.product_id
+    ORDER BY order_id
 )
 
 SELECT * FROM final
--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -1,33 +1,32 @@
-{{ config(materialized="view") }}
-
-with
-    src_promos as (select * from {{ ref("base_sql_server_dbo__promos") }}),
-
-    renamed_casted as (
-        select
-            md5(promo_id) as promo_id,
-            promo_id as promo_name,
-            case
-                when status like 'inactive' then 0 when status like 'active' then 1
-            end as status_id,
-            discount_dollars,
-            coalesce(_fivetran_deleted, 0) as _fivetran_deleted,
-            convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
-        from src_promos
+WITH src_promos AS (
+    SELECT *
+    FROM {{ source('sql_server_dbo', 'promos') }}
     ),
-     new_row as (
-        select
-            md5('no promo') as promo_id,
-            'no promo' as promo_name,
-            1 as status_id,
-            0 as discount_dollars,
-            false as _fivetran_deleted,
-            convert_timezone('UTC', CURRENT_DATE())  as _fivetran_synced
-        from src_promos
-    )
 
-select *
-from renamed_casted
-union
-select *
-from new_row
+renamed_casted AS (
+    SELECT
+            {{dbt_utils.generate_surrogate_key(['promo_id'])}} as promo_id,
+            promo_id AS promo_name,
+            discount AS discount_dollars,
+            CASE   
+            WHEN status LIKE 'active' then 1
+            WHEN status LIKE 'inactive' then 0
+            END AS status_id,
+            coalesce(_fivetran_deleted, false) AS date_deleted,
+            convert_timezone('UTC',_fivetran_synced) AS date_load
+    FROM src_promos a
+    ),
+
+new_row as (
+    select
+        {{dbt_utils.generate_surrogate_key(["'no promo'"])}} as promo_id,
+        'no promo' as promo_name,
+        0 as discount_dollars,  
+        1 as status_id,  
+        false as date_deleted, 
+        convert_timezone('UTC', current_timestamp()) as date_load_UTC  
+)
+
+SELECT * FROM renamed_casted
+UNION ALL
+SELECT * FROM new_row
